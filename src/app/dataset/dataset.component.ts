433481ad4c7ec18e159946c3bad7f13498c18750
aditinikam <aditirnikam21@gmail.com>
2021-01-20 20:41:07 +0530

    Dataset component working

--- src/app/dataset/dataset.component.ts
+++ src/app/dataset/dataset.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dataset',
@@ -6,19 +6,119 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dataset.component.css']
 })
 
-export class DatasetComponent implements OnInit {
+export class DatasetComponent implements OnInit,AfterViewInit {
 
   links = [
-    {data:'Sun'},
-    {data:'Flower'},
-    {data:'Pencil'},
-    {data:'Umbrella'},
-    {data:'House'},
-    {data:'Spoon'}
+    {id:'1',data:'Sun'},
+    {id:'2',data:'Flower'},
+    {id:'3',data:'Pencil'},
+    {id:'4',data:'Umbrella'},
+    {id:'5',data:'House'},
+    {id:'6',data:'Spoon'}
   ];
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    const canvas = document.getElementById("myCanvas") as HTMLFormElement;
+    const ctx = canvas.getContext('2d');
+    ctx.font = "25px Courier New";
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#06067e";
+    ctx.fillText("Draw Inside this Canvas!",120,40);
   }
-
+  ngAfterViewInit(){
+    const canvas = document.getElementById("myCanvas") as HTMLFormElement;
+    const ctx = canvas.getContext('2d');
+    var flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+    var x = "black",
+    y = 2;
+    canvas.onmousemove=function (e) {
+      findxy('move', e)
+    }
+    canvas.onmousedown= function (e) {
+        findxy('down', e)
+    }
+    canvas.onmouseup = function (e) {
+        findxy('up', e)
+    }
+    canvas.onmouseout= function (e) {
+        findxy('out', e)
+    }
+    function findxy(res:string, e:any) {
+      if (res == 'down') {
+          console.log(e);
+          prevX = currX;
+          prevY = currY;
+          currX = e.offsetX;
+          currY = e.offsetY;
+  
+          flag = true;
+          dot_flag = true;
+          if (dot_flag) {
+              ctx.beginPath();
+              ctx.fillStyle = x;
+              ctx.fillRect(currX, currY, 2, 2);
+              ctx.closePath();
+              dot_flag = false;
+          }
+      }
+      if (res == 'up' || res == "out") {
+          flag = false;
+      }
+      if (res == 'move') {
+          if (flag) {
+              prevX = currX;
+              prevY = currY;
+              currX = e.offsetX;
+              currY = e.offsetY;
+              draw();
+          }
+      }
+    }
+    function draw() {
+      ctx.beginPath();
+      ctx.moveTo(prevX, prevY);
+      ctx.lineTo(currX, currY);
+      ctx.strokeStyle = x;
+      ctx.lineWidth = y;
+      ctx.stroke();
+      ctx.closePath();
+    }
+    function color(obj:any):void {
+      switch (obj.id) {
+          case "green":
+              x = "green";
+              break;
+          case "blue":
+              x = "blue";
+              break;
+          case "red":
+              x = "red";
+              break;
+          case "yellow":
+              x = "yellow";
+              break;
+          case "orange":
+              x = "orange";
+              break;
+          case "black":
+              x = "black";
+              break;
+          case "white":
+              x = "white";
+              break;
+      }
+      if (x == "white") y = 14;
+      else y = 2;
+    }
+  }
 }
+
